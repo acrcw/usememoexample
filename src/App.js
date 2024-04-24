9a30e5105b58cb9636f15dffadf880090937d656
joban <91016674+acrcw@users.noreply.github.com>
2024-04-24 12:56:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import Parent from "./Parent";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>hi</h1>
+      <Parent />
+    </div>
+  );
+}
